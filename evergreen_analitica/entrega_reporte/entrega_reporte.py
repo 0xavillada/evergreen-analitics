5c085a2ab8c9ed7a939b9e0b5a19ee8ac8636bec
chosky <davidhenaoo98@gmail.com>
2023-03-22 14:01:05 -0500

    [EG-ANA-004][BE] Se añade el gitignore y el template para flask y se hace la primera prueba y desarrollo del metodo de descarga de reporte

--- evergreen_analitica/entrega_reporte/entrega_reporte.py
+++ evergreen_analitica/entrega_reporte/entrega_reporte.py
@@ -6,6 +6,13 @@ import json, os, requests
 
 entrega_reporte_micro_service = Blueprint("entrega_reporte_micro_service", __name__)
 
+@entrega_reporte_micro_service.route('/api/obtener_archivos', methods=['GET'])
+def obtener_archivos():
+    print("Obteniendo los archivos")
+    archivos = [{'nombre': 'asdfasdf', 'url': 'http://asdfas.cas'}, {'nombre': 'qwerqw', 'url': 'http://qwerq.cas'}]
+    return jsonify({'archivos': archivos})
+
+
 @entrega_reporte_micro_service.route('/api/entrega_reporte_xlsx', methods=['POST'])
 def entrega_reporte_xlsx():
     print("entrega reporte xlsx")
